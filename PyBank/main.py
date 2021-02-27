e953384f2b99f6dad5bebe51305fc9ad45ac5cc5
Ken <kensikes988@gmail.com>
2021-02-26 18:01:30 -0800

    committing to not lose stuff

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,11 +1,48 @@
+#import modules
 import os
 import csv
 
+#create some variables
+total_months = 0
+month_counter = 0
+net_total = 0
+average_change = 0
+greatest_decrease = 0
+greatest_increase = 0
+total_profit = 0
+total_losses = 0
+
+
+
+#create file path
 csvpath = os.path.join("Resources", "budget_data.csv")
 
-
+#this allows you to read the file 
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ",")
+    #store headers here 
+    headers = next (csvreader)
+    
+    #count months
+    for row in csvreader: 
+        month_counter += 1
+        # total proft
+        net_total += row[1]
+        
+        
+        
+        
+        
 
-    for row in csvreader:
-        print(row)
+
+# changes over period
+average_change = net_total / month_counter        
+
+
+
+
+
+
+
+
+
