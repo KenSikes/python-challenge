a8e15582ead27b88e9fdbc62a342b211c684af74
Ken <kensikes988@gmail.com>
2021-02-27 20:56:46 -0800

    gettin committy wit it

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,7 +2,23 @@
 import os
 import csv
 
-#here lies the variables
+#here lie the variables
+vote_counter = 0
+total_votes = 0
+#variables to track candidates and their votes 
+khan_votes = 0
+correy_votes = 0
+li_votes = 0
+otooley_votes = 0
+#variables to track their percent of votes
+percent_khan = 0
+percent_correy = 0
+percent_li = 0
+percent_otooley = 0 
+winner = ""
+
+
+
 
 
 #create file path
@@ -14,9 +30,19 @@ with open(csvpath) as csvfile:
     #store headers here 
     headers = next (csvreader)
 
-    for row in csvfile:
-        print(row)
+    #count votes
+    for row in csvreader:
+        vote_counter += 1
+        #count votes for each candidate
+        if row[2] == 'Khan':
+            khan_votes += 1
+        elif row[2] == 'Correy':
+            correy_votes += 1
+        elif row[2] == 'Li':
+            li_votes += 1
+        elif row[2] == "O'tooley":
+            otooley_votes += 1
 
-#hello 
+
 
 
